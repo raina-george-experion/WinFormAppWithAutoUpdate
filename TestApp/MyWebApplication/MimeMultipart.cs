10cf13ffeb40fcc0fc41ada22328121d6b3b7b9c
raina.george@experionglobal.com <RrAaSs@54>
2022-11-22 11:37:21 +0530

    winform app

---
+++ TestApp/MyWebApplication/MimeMultipart.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace MyWebApplication
+{
+    public class MimeMultipart : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(
+                    new HttpResponseMessage(
+                        HttpStatusCode.UnsupportedMediaType)
+                );
+            }
+        }
+    }
+}
